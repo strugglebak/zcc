6fb15dff4117bea706aa57155832126ac88b5fed
strugglebak <strugglebak@gmail.com>
2021-04-06 19:38:06 +0800

    fix: 如果是 Linux 系统而不是 Unix Mac 系统，那么最好不要在函数前面加下划线

--- generator_core.c
+++ generator_core.c
@@ -59,17 +59,17 @@ void register_preamble() {
     // MacOS & iOS 不用 PLT
     // https://stackoverflow.com/questions/59817831/unsupported-symbol-modifier-in-branch-relocation-call-printfplt
     // "\tcall	_printf@PLT\n"
-    "\tcall	_printf\n"
+    "\tcall	printf\n"
     "\tnop\n"
     "\tleave\n"
     "\tret\n"
     "\n"
-    "\t.globl\t_main\n"
+    "\t.globl\tmain\n"
     // 如果是要编译成 Mach-O x86-64 的汇编，则不需要下面的这个指令，因为这个指令为一个调试用的指令，通常用来调用桢信息的
     // 详情见
     // https://stackoverflow.com/questions/19720084/what-is-the-difference-between-assembly-on-mac-and-assembly-on-linux/19725269#19725269
     // "\t.type\tmain, @function\n"
-    "_main:\n"
+    "main:\n"
     "\tpushq\t%rbp\n"
     "\tmovq	%rsp, %rbp\n",
     output_file
