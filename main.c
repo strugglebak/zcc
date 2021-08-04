bc5bd15a43e361d9b942681d1eb3863ab399cf02
strugglebak <strugglebak@gmail.com>
2021-08-04 21:35:48 +0800

    fix: 修改 main.c

--- main.c
+++ main.c
@@ -49,19 +49,12 @@ int main(int argc, char *argv[]) {
 
   // 扫描文件中的字符串，并将其赋值给 token_from_file 这个全局变量
   scan(&token_from_file);
-  // // 开始在解析的过程中，把字符串转换成 ast
-  // tree = converse_token_2_ast(0);
-  // // 再将这颗树解析，并输出结果
-  // result = interpret_ast(tree);
-  // printf("Expression result = %d\n", result);
-
-  // // 生成汇编代码
-  // generate_code(tree);
 
   // 生成汇编代码
   // 这里主要是测试有 print 的语句的情况
   generate_preamble_code();
-  parse_compound_statement();
+  tree = parse_compound_statement();
+  interpret_ast_with_register(tree, NO_REGISTER, 0);
   generate_postamble_code();
 
   // 关闭文件
