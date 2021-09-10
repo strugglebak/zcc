4e60b3127de7d07bb6212c95ca52dff09d1cb194
strugglebak <strugglebak@gmail.com>
2021-09-10 22:56:55 +0800

    chore: 删除无用代码

--- types.c
+++ types.c
@@ -5,52 +5,6 @@
 #include "types.h"
 #include "ast.h"
 
-// 比较俩基本类型是否兼容
-// 返回 1 为兼容，0 为不兼容
-
-// 如果 only_right 为 true，则表示只能从 left 扩展到 right
-// 比如对于 char i = 1; int j = i; 这种情况
-// 如果此时 stop_type_to_widen 是 true, 那么将不会出现强制转换
-int check_type_compatible(
-  int *left_primitive_type,
-  int *right_primitive_type,
-  int stop_type_to_widen
-) {
-  int left_primitive_type_size, right_primitive_type_size;
-
-  // 检查是否一致
-  if (*left_primitive_type == *right_primitive_type) {
-    *left_primitive_type = *right_primitive_type = 0;
-    return 1;
-  }
-
-  // 用 size 来检查是否一致
-  left_primitive_type_size = register_get_primitive_type_size(*left_primitive_type);
-  right_primitive_type_size = register_get_primitive_type_size(*right_primitive_type);
-
-  if (left_primitive_type_size == 0 ||
-      right_primitive_type_size == 0)
-      return 0;
-
-
-  // 检查不一致时的处理
-  if (left_primitive_type_size < right_primitive_type_size) {
-    *left_primitive_type = AST_WIDEN;
-    *right_primitive_type = 0;
-    return 1;
-  }
-  if (left_primitive_type_size > right_primitive_type_size) {
-    if (stop_type_to_widen) return 0;
-    *left_primitive_type = 0;
-    *right_primitive_type = AST_WIDEN;
-    return 1;
-  }
-
-  // 剩下的都是兼容的
-  *left_primitive_type = *right_primitive_type = 0;
-  return 1;
-}
-
 int check_int_type(int primitive_type) {
   if (primitive_type == PRIMITIVE_CHAR ||
       primitive_type == PRIMITIVE_INT ||
--- types.h
+++ types.h
@@ -1,11 +1,6 @@
 #ifndef __TYPES_H__
 #define __TYPES_H__
 
-int check_type_compatible(
-  int *left_primitive_type,
-  int *right_primitive_type,
-  int stop_type_to_widen
-);
 int pointer_to(int primitive_type);
 int value_at(int primitive_type);
 int check_int_type(int primitive_type);
