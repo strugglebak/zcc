09f6eb76b1c2ae1e2e0f4d3e3484375128c8518b
strugglebak <strugglebak@gmail.com>
2021-09-13 14:47:40 +0800

    fix: 删除多余代码

--- statement.c
+++ statement.c
@@ -34,52 +34,6 @@ static struct ASTNode *parse_single_statement() {
   }
 }
 
- struct ASTNode *parse_assignment_statement() {
-  struct ASTNode *tree, *left, *right;
-  int symbol_table_index;
-  int left_primitive_type, right_primitive_type = 0;
-
-  // 先解析是不是一个标识符
-  verify_identifier();
-
-  // 解析类似的语句时需要注意的问题，即区别是变量名还是函数调用
-  // fred = 2;
-  // fred(18);
-
-  // 如果是左 (，则直接看作是函数调用
-  if (token_from_file.token == TOKEN_LEFT_PAREN)
-    return convert_function_call_2_ast();
-
-  // 找下这个标识符有没有重复定义过
-  // 如果没被定义就用了，抛出异常
-  if ((symbol_table_index = find_global_symbol_table_index(text_buffer)) == -1) {
-    error_with_message("Undeclared variable", text_buffer);
-  }
-
-  // 创建右节点
-  // 这里将左值塞到右节点的目的是在将值保存到变量之前，优先计算 statement 的值
-  // 注意我们这里的左节点的操作优先级高于右节点
-  right = create_ast_leaf(
-    AST_LVALUE_IDENTIFIER,
-    global_symbol_table[symbol_table_index].primitive_type,
-    symbol_table_index);
-
-  // 解析 statement 中是否有 = 号
-  verify_token_and_fetch_next_token(TOKEN_ASSIGN, "=");
-
-  // 解析 statement（也就是右值），并创建左节点
-  left = converse_token_2_ast(0);
-
-  // 检查类型是否兼容
-  left = modify_type(left, right->primitive_type, 0);
-  if (!left) error("Incompatible expression in assignment");
-
-  // 根据左右节点创建一颗有关 statement 的 ast 树
-  tree = create_ast_node(AST_ASSIGN, PRIMITIVE_INT, left, NULL, right, 0);
-
-  return tree;
-}
-
 struct ASTNode *parse_if_statement() {
   struct ASTNode *condition_node, *true_node, *false_node = NULL;
 
--- statement.h
+++ statement.h
@@ -1,7 +1,6 @@
 #ifndef __STATEMENT_H__
 #define __STATEMENT_H__
 
-struct ASTNode *parse_assignment_statement();
 struct ASTNode *parse_if_statement();
 struct ASTNode *parse_while_statement();
 struct ASTNode *parse_for_statement();
