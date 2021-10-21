1223ee0e34c8b4f3e570947261ab3d7591b19682
strugglebak <strugglebak@gmail.com>
2021-10-21 21:58:02 +0800

    feat: 增加常量折叠函数

---
+++ optimizer.c
@@ -0,0 +1,65 @@
+#include "data.h"
+#include "definations.h"
+#include "declaration.h"
+#include "ast.h"
+
+static struct ASTNode *fold_2_children(struct ASTNode *node) {
+  int value, left_value, right_value;
+
+  left_value = node->left->ast_node_integer_value;
+  right_value = node->right->ast_node_integer_value;
+
+  switch (node->operation) {
+    case AST_PLUS:
+      value = left_value + right_value;
+      break;
+    case AST_MINUS:
+      value = left_value - right_value;
+      break;
+    case AST_MULTIPLY:
+      value = left_value * right_value;
+      break;
+    case AST_DIVIDE:
+      if (!right_value) return node;
+      value = left_value / right_value;
+      break;
+    default:
+      return node;
+  }
+
+  return create_ast_leaf(AST_INTEGER_LITERAL, node->primitive_type, value, NULL);
+}
+
+static struct ASTNode *fold_1_children(struct ASTNode *node) {
+  int value;
+
+  value = node->left->ast_node_integer_value;
+
+  switch (node->operation) {
+    case AST_WIDEN: break;
+    case AST_INVERT: value = ~value; break;
+    case AST_LOGIC_NOT: value = !value; break;
+    default: return node;
+  }
+
+  return create_ast_leaf(AST_INTEGER_LITERAL, node->primitive_type, value, NULL);
+}
+
+static struct ASTNode *fold(struct ASTNode *node) {
+  if (!node) return node;
+
+  node->left = fold(node->left);
+  node->right = fold(node->right);
+
+  if (node->left && node->left->operation == AST_INTEGER_LITERAL) {
+    if (node->right && node->right->operation == AST_INTEGER_LITERAL)
+      node = fold_2_children(node);
+  } else
+    node = fold_1_children(node);
+
+  return node;
+}
+
+struct ASTNode *optimise(struct ASTNode *node) {
+  return fold(node);
+}
---
+++ optimizer.h
@@ -0,0 +1,6 @@
+#ifndef __OPTIMIZER_H__
+#define __OPTIMIZER_H__
+
+struct ASTNode *optimise(struct ASTNode *node);
+
+#endif
