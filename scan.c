3ae63bd157ae2ff0da30e3c7ce8d60fdcc3dc385
strugglebak <strugglebak@gmail.com>
2022-02-06 14:14:00 +0800

    Revert "fix: 使用宏定义调试代码"
    This reverts commit 92e99506db77f853088bf6c31baa3e9a39cb5108.

--- scan.c
+++ scan.c
@@ -496,8 +496,6 @@ int scan(struct Token *t) {
   }
 
   t->token_string = token_string[t->token];
-#define DEBUG 1
-#if DEBUG == 1
   printf(
     "scan '%s' '%s' -> (%s), line = %d\n",
     global_input_filename,
@@ -505,6 +503,5 @@ int scan(struct Token *t) {
     text_buffer,
     line
   );
-#endif
   return (1);
 }
